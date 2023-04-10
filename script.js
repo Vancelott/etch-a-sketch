b6967e27278d9cb4675ab325aed4a2f7a2b828a5
Vancelott <ivan.angelovv2@gmail.com>
2023-04-10 12:03:46 +0300

    Progressed a bit through the project, added the hover function, grid size button still not finished

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+const mouseHover = document.getElementById("mouseHover");
+const sizeButton = document.querySelector("#size");
+
 const createGrid = (n) => {
   const squaresContainer = document.querySelector('.squares');
-  for (let i = 0; i < 32; i++) {
+  for (let i = 0; i < n * n; i++) {
     squaresContainer.appendChild(
       Object.assign(
-        document.createElement('div'), {
+        document.createElement('div'),
+        mouseHover.appendChild(squaresContainer)))
+        squaresContainer.addEventListener('mouseover', function(event) {
+          event.target.style.backgroundColor = "black"
         }
       )
-    );
   }
 }
 
-createGrid();
 
+function gridSize() {
+  const size = prompt("Please enter grid size");
+  createGrid(size)
+}
+
+sizeButton.addEventListener("click", gridSize);
+
+// n = 16
+
+createGrid(16);
